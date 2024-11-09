1ba71d80b40d2a6cdcf69302a4c2b67eeabc9997
chocho030 <chocho0003@naver.com>
2024-11-09 12:19:34 +0900

    복수의 변수 출력

---
+++ c-language-basic/c-language-basic/prob1.c
@@ -0,0 +1,13 @@
+﻿#include <stdio.h>
+
+int main() {
+	// 3개의 정수 변수를 초기화 한 뒤에 
+	// 한줄의 printf 문 안에서 3개의 변수를 출력해주세요 
+	// (힌트 : ,를 추가해줄것)
+
+	int num1 = 1, num2 = 2, num3 = 3;
+
+	printf("%d %d %d", num1, num2, num3);
+	printf("Hello world");
+
+}
