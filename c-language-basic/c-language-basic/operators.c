d46df1ca54f85bc9c93e0d7b837d243526501a36
chocho030 <chocho0003@naver.com>
2024-11-12 22:36:59 +0900

    연산자 실습 파일 추가

---
+++ c-language-basic/c-language-basic/operators.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+// 연산자 실습
+int main() {
+	// 산술 연산
+	int a = 20;
+	int b = 10;
+
+	printf("%d + %d = %d \n",a, b, a + b);
+	printf("%d - %d = %d \n",a, b, a - b);
+	printf("%d * %d = %d \n",a, b, a * b);
+	printf("%d / %d = %d \n",a, b, a / b);
+	printf("%d %% %d = %d \n",a, b, a % b);
+
+
+
+
+
+
+
+}
